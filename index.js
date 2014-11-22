6fed3ada6a9c237858b7090a9b8658a3a11c8e2d
Simon Sun <blackghost0118@gmail.com>
2014-11-22 12:52:36 +0800

    add start function

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+/*
+ * node modules
+ */
 var async = require('async');
+var CronJob = require('cron').CronJob;
 
+/*
+ * setting cron jobs
+ */
+var job1 = new CronJob({
+  cronTime: '*/5 * * * * *',
+  onTick: function() {
+    console.log(5);
+  },
+  start: false,
+  timeZone: "Asia/Taipei"
+});
+
+/*
+ * start function
+ */
 var start = function (){
-	console.log('start');
+	job1.start();
+
 };
 
+/*
+ * start
+ */
 start();
