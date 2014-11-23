09c7f71eb0b3b9c4619d5fbe8241bf2089b4658b
Simon Sun <blackghost0118@gmail.com>
2014-11-23 12:20:30 +0800

    fuck

--- controllers/buy.js
+++ controllers/buy.js
@@ -8,6 +8,8 @@ var jsdom = require('jsdom');
 var iconv = require('iconv-lite');
 var fs = require('fs');
 var http = require('http');
+var cheerio = require('cheerio');
+var webshot = require('webshot');
 
 var models = require('../models');
 var libs = require('../libs');
@@ -36,21 +38,6 @@ module.exports = function(next) {
 						returnTicket: '0'
 					};
 
-					// request({
-					// 	url: 'http://railway.hinet.net/coquery.jsp?personId=A191240659&orderCode=955793',
-					// 	encoding: null,
-					// 	headers: {
-					// 		referer: 'http://railway.hinet.net/coquery.htm'
-					// 	}
-					// }, function(err, res, body) {
-					// 	console.log(err);
-					// 	console.log(res);
-					// 	var str = iconv.decode(new Buffer(body), "big5");
-					// 	console.log(str);
-
-					// });
-
-
 					request.post({
 						url: 'http://railway.hinet.net/check_ctno1.jsp',
 						encoding: null,
@@ -65,44 +52,70 @@ module.exports = function(next) {
 						}
 
 						var setCookie = httpResponse.headers['set-cookie'].join(';');
-						console.log(body);
 						var str = iconv.decode(new Buffer(body), "big5");
+
+						$ = cheerio.load(str);
+						var captchaUrl = $('#idRandomPic').attr('src');
+
+						webshot('railway.hinet.net/ImageOut.jsp;' + captchaUrl, 'hello_world.png', function(err) {
+							console.log(err);
+						});
+
 
 						libs.captcha.credentials = {
 							username: 'SimonSun',
 							password: '19880118'
 						};
 
-						var file = fs.createWriteStream("file.jpg");
-						var getImage = http.get("http://railway.hinet.net/ImageOut.jsp", function(response) {
-						  response.pipe(file);
-						    file.on('finish', function() {
-						      file.close(function(){
-								libs.captcha.decodeFile('file.jpg', 10000, function(err, result) {
-									// console.log(result.text);
-									var url = 'http://railway.hinet.net/order_no1.jsp?randInput=' + result.text + '&person_id=R123840258&getin_date=2014/11/24-01&from_station=100&to_station=185&order_qty_str=1&train_no=553&returnTicket=0';
-									// console.log(url);
-									// console.log(typeof url);
-									request({
-										url: url,
-										encoding: null,
-										headers: {
-											referer: 'http://railway.hinet.net/check_ctno1.jsp',
-											cookie: setCookie
-										}
-									},
-									function (err, res, body){
-										if(err){
-											console.log(err);
-										}
-										// console.log(body);
-										var str = iconv.decode(new Buffer(body), "big5");
-										console.log(str);
-									});
+						libs.captcha.decodeFile('hello_world.png', 10000, function(err, result) {
+							console.log(result.text);
+							var url = 'http://railway.hinet.net/order_no1.jsp?randInput=' + result.text + '&person_id=R123840258&getin_date=2014/11/24-01&from_station=100&to_station=185&order_qty_str=1&train_no=553&returnTicket=0';
+							request({
+									url: url,
+									encoding: null,
+									headers: {
+										referer: 'http://railway.hinet.net/check_ctno1.jsp',
+										cookie: setCookie
+									}
+								},
+								function(err, res, body) {
+									if (err) {
+										console.log(err);
+									}
+
+									var str = iconv.decode(new Buffer(body), "big5");
+									console.log(str);
 								});
-						      });  // close() is async, call cb after close completes.
-						    });
 						});
+
+
+						// var file = fs.createWriteStream("file.jpg");
+						// var getImage = http.get("http://railway.hinet.net/ImageOut.jsp", function(response) {
+						// 	response.pipe(file);
+						// 	file.on('finish', function() {
+						// 		file.close(function() {
+						// 			libs.captcha.decodeFile('file.jpg', 10000, function(err, result) {
+						// 				var url = 'http://railway.hinet.net/order_no1.jsp?randInput=' + result.text + '&person_id=R123840258&getin_date=2014/11/24-01&from_station=100&to_station=185&order_qty_str=1&train_no=553&returnTicket=0';
+						// 				request({
+						// 						url: url,
+						// 						encoding: null,
+						// 						headers: {
+						// 							referer: 'http://railway.hinet.net/check_ctno1.jsp',
+						// 							cookie: setCookie
+						// 						}
+						// 					},
+						// 					function(err, res, body) {
+						// 						if (err) {
+						// 							console.log(err);
+						// 						}
+
+						// 						var str = iconv.decode(new Buffer(body), "big5");
+						// 						console.log(str);
+						// 					});
+						// 			});
+						// 		});
+						// 	});
+						// });
 					});
 
 					callback()
